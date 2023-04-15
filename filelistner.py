22af0dd33c3cd7aa710f8faee41517ae0b38ed8b
BJune <Bejuney@gmail.com>
2023-04-14 21:34:03 -0400

    Improved main.py A BUNCH

--- filelistner.py
+++ filelistner.py
@@ -1,13 +1,17 @@
-import pythoncom
-import wmi
+######################
+#NOT USED
+######################
 
-import jsonhandler
-
-def programlistner():
-    targetApplication = jsonhandler.read("config.json")["data"]["applicationToListenForCaption"]
-
-    pythoncom.CoInitialize() #init libaries
-    c = wmi.WMI()
-
-    for _ in c.Win32_Process(name=targetApplication): #for every process open, if it has the proper name, return true
-        return True
+#import pythoncom
+#import wmi
+#
+#import jsonhandler
+#
+#def programlistner():
+#    targetApplication = jsonhandler.read("config.json")["data"]["applicationToListenForCaption"]
+#
+#    pythoncom.CoInitialize() #init libaries
+#    c = wmi.WMI()
+#
+#    for _ in c.Win32_Process(name=targetApplication): #for every process open, if it has the proper name, return true
+#        return True
